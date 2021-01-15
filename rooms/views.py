f7e9e58d3cb105292f594b0f841b5801e4c8dd91
BDARJH <xzdfor@naver.com>
2021-01-15 17:07:56 +0900

    ~#11.5 Pagination Study

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator  # 장고가 도와주는
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"potato": all_rooms})
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)  # 오브젝트 목록, 한페이지당 나오는 오브젝트 개수
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"page", rooms})
